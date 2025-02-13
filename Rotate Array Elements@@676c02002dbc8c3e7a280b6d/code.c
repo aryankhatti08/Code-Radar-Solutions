d52831844047fb5a68d81c964d97e0ae30d22d61
Aryan <aryan0056.becse24@chitkara.edu.in>
2025-02-13 10:09:17 +0530

    Solution to Rotate Array Elements

---
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int isPrime(int n) {
+    if (n < 2) return 0;
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0) return 0;
+    return 1;
+}
+
+int main() {
+    int t, num;
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d", &num);
+        printf("%d\n", isPrime(num));
+    }
+    return 0;
+}
