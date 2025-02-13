4e5afb1ac3867ce820b5bfb75d2dc0a65f3b9685
Aryan <aryan0056.becse24@chitkara.edu.in>
2025-02-13 09:59:09 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -1,23 +1,36 @@
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 
-int main(void)
+int main(int argc, const char * argv[])
 {
-    printf("Enter a number to be checked\n");
-    long long int value;
-    scanf("%lld", &value);
-    long long int stop = round(sqrt(value));
-
-    for (int i = 2; i < stop + 1; i++)
-    {
-        if (value % i == 0)
-        {
-            printf("Not prime\n");
-            exit(0);
-        }         
-        else continue;
+    long numCheck = 0;
+    int divisibleByCounter = 2;
+    printf("Enter a number: \n");
+    scanf("%ld", &numCheck);
+    long halfNumCheck = numCheck / 2;
+    
+    if(numCheck == 1){
+        printf("The number 1 is not a prime! A prime number must satisfy two conditions: it should be divisible by 1 and by itself");
+    } else {
+    if(numCheck < 1){
+        printf("Please enter a number greater than 0");
+    } else {
+        printf("The number %ld can be divided by: \n1", numCheck);
+        for(long i = 2; i < halfNumCheck + 1;){
+            if(numCheck % i == 0){
+                divisibleByCounter++;
+                printf("\n%ld", i);
+                }
+            i++;
+        }
+        printf("\n%ld", numCheck);
+        
+        if(divisibleByCounter == 2){
+            printf("\nThe number '%ld' is a prime! It can only be divided by 1 and %ld", numCheck, numCheck);
+        } else {
+            printf("\nThe number '%ld' is not a prime, it has %d divisors", numCheck, divisibleByCounter);
+        }
     }
-    printf("Is prime\n");
-    exit(0);
+    }
+    return 0;
 }
